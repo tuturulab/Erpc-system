6ed43c9afaab6511a8187bbb2febb5be01130737
Administrator <lagunamichael2@outlook.com>
2019-08-21 00:01:28 -0600

    fix merge

--- src/components/Root.js
+++ src/components/Root.js
@@ -5,12 +5,9 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 
 import Home from './Home';
 import NotFoundPage from './404';
-//import Main from './Dashboard/Main';
+import Main from './Dashboard/Main';
 
 //import './Dashboard/styles/index.scss';
-const About = () => <div><h3>About me!</h3></div>;
-const Admin = () => <div><h1>Admin template!</h1></div>;
-import Main from './Dashboard/Main';
 
 const Root = ({ store }) => (
   <Provider store={store}>
@@ -18,7 +15,7 @@ const Root = ({ store }) => (
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/home" /> } />
         <Route path="/home" component={Home} />
-        <Route path="/admin" component={Admin} />
+        <Route path="/admin" component={Main} />
         <Route component={NotFoundPage}/>
       </Switch>
     </Router>
